80ea5479dc5f2702fa1e0c5b1dc42f8b8aa8d36d
Wesley_Ferreira <wesrga94@gmail.com>
2025-08-15 19:42:56 -0300

    Limpeza dos comentários e preparação apra o deploy

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -5,26 +5,6 @@ interface Props {
   data: WeatherData;
 }
 
-/*const WeatherCard: React.FC<Props> = ({ data }) => {
-  const { name, main, weather } = data;
-  const { temp } = main;
-  //const { description, icon } = weather[0];
-  const { description, icon } = weather?.[0] ?? { description: '', icon: '' };
-
-
-  return (
-    <div style={{ marginTop: '20px' }}>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>Temperatura: {temp}°C</p>
-      <img
-        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-        alt="Ícone do clima"
-      />
-    </div>
-  );
-};*/
-
 const WeatherCard: React.FC<Props> = ({ data }) => {
   const { location, current } = data;
   const { name, country } = location;

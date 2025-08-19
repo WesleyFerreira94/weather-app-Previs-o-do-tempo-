5366cd6302835a21d794aa2e55238c355cb06889
Wesley Cristian Ferreira Leite <wesrga94@gmail.com>
2025-08-19 00:40:38 -0300

    Update weather.ts

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,3 +1,7 @@
+// Define o formato dos dados do clima que vêm da API
+// Inclui o nome da cidade, país, temperatura atual e descrição do tempo
+// Usado para garantir que o TypeScript reconheça essas informações corretamente
+
 export interface WeatherData {
   location: {
     name: string;

cfa224150d01eac1aba7e310637fafdc77f932be
Wesley Cristian Ferreira Leite <wesrga94@gmail.com>
2025-08-19 00:35:19 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
+// Declarações de tipos para variáveis de ambiente do Vite.
+// Garante que o TypeScript reconheça corretamente as variáveis usadas no projeto.
+
 /// <reference types="vite/client" />
